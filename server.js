4b0575a3d99b3c4eec5895836040bd5f724a23dc
Brian Sang <slamsang@gmail.com>
2021-03-23 19:15:45 +0900

    update

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-const PORT = 80;
+//const PORT = 9000;
+const PORT = process.env.PORT || 9000
 
 app.use(express.static('dist'));
 app.use(express.static('./'));
@@ -10,5 +11,9 @@ app.get('/', function(req, res) {
   res.sendfile('index.html');
 });
 
-app.listen(PORT);
+//app.listen(PORT);
+app.listen(PORT, err => {
+  if(err) throw err;
+  console.log("%c Server running", "color: green");
+});
 console.log(`[SERVER RUNNING] 127.0.0.1:${PORT}`);
